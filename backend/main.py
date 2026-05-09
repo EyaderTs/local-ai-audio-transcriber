ccb390ce9c4c91229ad7f4c538bbae99e2ff664c
Eyader Tsehayu <eyadertsehayu@gmail.com>
2026-05-09 21:57:41 +0300

    Implement audio transcription endpoint with temporary file handling - trial

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI,UploadFile
-
+import tempfile
+import os
 
 app= FastAPI()
 
+
+@app.post("/transcribe")
+async def transcribe_audio(audio:UploadFile):
+
+    content = await audio.read()
+
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        tmp.write(content)
+        tmp_path = tmp.name
+    
+    try:     
+        return{
+            "name":audio.filename,
+            "temp_file":tmp_path    
+        }
+
+    finally:
+        if os.path.exists(tmp_path):
+            os.unlink(tmp_path)
     
 @app.get("/")
 def health():
