a81be730958ea471131adb53bd3ba6cd2b17859f
Eyader Tsehayu <eyadertsehayu@gmail.com>
2026-05-10 14:46:56 +0300

    Refactor audio transcription endpoint and improve model loading messages

--- backend/main.py
+++ backend/main.py
@@ -1,29 +1,49 @@
-from fastapi import FastAPI,UploadFile
+from fastapi import FastAPI, UploadFile
+from faster_whisper import WhisperModel
 import tempfile
 import os
 
-app= FastAPI()
+app = FastAPI()
+
+print("Loading Whisper model... Please wait.")
+
+model = WhisperModel(
+    "tiny",
+    device="auto",
+    compute_type="int8"
+)
+
+print("Whisper model fully loaded!")
 
 
 @app.post("/transcribe")
-async def transcribe_audio(audio:UploadFile):
+async def transcribe_audio(audio: UploadFile):
 
     content = await audio.read()
 
     with tempfile.NamedTemporaryFile(delete=False) as tmp:
+
         tmp.write(content)
         tmp_path = tmp.name
-    
-    try:     
-        return{
-            "name":audio.filename,
-            "temp_file":tmp_path    
+
+    try:
+
+        segments, _ = model.transcribe(tmp_path)
+
+        text = " ".join(
+            segment.text for segment in segments
+        )
+
+        return {
+            "text": text
         }
 
     finally:
+
         if os.path.exists(tmp_path):
             os.unlink(tmp_path)
-    
+
+
 @app.get("/")
 def health():
-    return {"Message":"Hello from FastAPI"}
+    return {"message": "Hello from FastAPI"}
